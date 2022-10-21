03c455d919a301a34ef609a6d7a79cfcf89519cb
eriks junior <eriksjunior13@gmail.com>
2022-10-21 00:32:34 -0300

    feat: criado service de clientes

---
+++ src/interface/ICliente.ts
@@ -0,0 +1,18 @@
+export interface ICliente {
+  id: string
+  idEmpresa: string
+  nome: string
+  cpfCnpj: string
+  endereco: string
+  numero: string
+  complemento: string
+  bairro: string
+  cidade: string
+  uf: string
+  cep: string
+  telefone: string
+  celular: string
+  email: string
+  dataNascimento: string
+  observacao: string
+}
--- src/services/ClienteService.ts
+++ src/services/ClienteService.ts
@@ -1,1 +1,26 @@
-import { api } from "../utils/configs/api";
+import { api } from '../utils/configs/api';
+import { ICliente } from '../interface/ICliente';
+export class Cliente {
+  async save(dataCliente: ICliente) {
+    const { data } = await api.post('/customers', dataCliente)
+    return data
+  }
+
+  async update(dataCliente: ICliente) {
+    await api.put(`/customers/${dataCliente.id}`, dataCliente)
+  }
+
+  async delete(id: string) {
+    await api.delete(`/customers/${id}`)
+  }
+
+  async getFromSelectBox() {
+    const { data } = await api.get('/customers/combobox')
+    return data
+  }
+
+  async search(q: string, page: number) {
+    const { data } = await api.get(`customers?q=${q}&page=${page}`)
+    return data
+  }
+}
