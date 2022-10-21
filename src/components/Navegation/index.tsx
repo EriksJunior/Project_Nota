0665b0c5172ff36fbe45baad4f52e0963823076c
eriks junior <eriksjunior13@gmail.com>
2022-10-21 00:53:20 -0300

    feat: instalado styled components e aplicado no componente de navigation

--- src/components/Navegation/index.tsx
+++ src/components/Navegation/index.tsx
@@ -1,10 +1,12 @@
-import {ClientModal} from '../Cliente/index'
-import {ProductsModal} from '../Products/index'
-export function Navegation(){
-    return(
-      <div style={{ display:"flex", width: "100%" , height: "60px", gap:"30px" , background:"#1C1C1C" , alignItems:"center"  }} >
-        <ClientModal/>
-        <ProductsModal/>
-      </div>
-    )
-  }
+import { ClientModal } from '../Cliente/index'
+import { ProductsModal } from '../Products/index'
+
+import { Div } from './styles'
+export function Navegation() {
+  return (
+    <Div>
+      <ClientModal />
+      <ProductsModal />
+    </Div>
+  )
+}
---
+++ src/components/Navegation/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Div = styled.div`
+  display: flex; 
+  width: 100%; 
+  height: 60px; 
+  gap: 30px; 
+  background: #1C1C1C; 
+  align-items: center;
+  padding: 10px;
+`
