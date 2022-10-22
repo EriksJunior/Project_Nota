ab499e8e59f29d9577a6f2ddd8f32b0df21e1f43
eriks junior <eriksjunior13@gmail.com>
2022-10-22 13:48:57 -0300

    feat: ajustes de estilos na nav

--- src/components/Navegation/index.tsx
+++ src/components/Navegation/index.tsx
@@ -2,13 +2,18 @@ import { Company } from '../../page/Company'
 import { ClientModal } from '../Cliente/index'
 import { ProductsModal } from '../Products/index'
 
-import { Div } from './styles'
+import { Container, Content, SettingsContent } from './styles'
 export function Navegation() {
   return (
-    <Div>
-      <ClientModal />
-      <ProductsModal />
-      <Company/>
-    </Div>
+    <Container>
+      <Content>
+        <ClientModal />
+        <ProductsModal />
+      </Content>
+
+      <SettingsContent>
+        <Company />
+      </SettingsContent>
+    </Container>
   )
 }
--- src/components/Navegation/styles.ts
+++ src/components/Navegation/styles.ts
@@ -1,11 +1,42 @@
 import styled from 'styled-components'
 
-export const Div = styled.div`
+export const Container = styled.div`
   display: flex; 
   width: 100%; 
-  height: 60px; 
-  gap: 30px; 
+  height: auto; 
   background: #1C1C1C; 
   align-items: center;
+  justify-content: space-between;
   padding: 10px;
+  gap: 20px;
+  
+  @media screen and (max-width: 361px){
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+`
+
+export const Content = styled.div`
+  width: 50%;
+  display: flex;
+  gap: 20px;
+
+  @media screen and (max-width: 361px){
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+`
+
+export const SettingsContent = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+
+  @media screen and (max-width: 361px){
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
 `
