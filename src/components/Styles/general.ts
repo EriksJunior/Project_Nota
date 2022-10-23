32e674445a89399d950356306dc6c4bb47d3866a
leobraboo <leonardo.l.souza@ufv.br>
2022-10-23 01:48:46 -0300

    FEATURE : layout do leaf

--- src/components/Styles/general.ts
+++ src/components/Styles/general.ts
@@ -7,3 +7,28 @@ export const ContentIcon = styled.div`
 export const Text = styled.div`
   margin-top: 5px;
 `
+
+export const ContextLeaf = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  padding: 10px;
+  align-items: center;
+  padding: 10px;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+`
+
+export const ButtonLeaf = styled.div`
+  display: flex;
+  align-items: center;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: space-between;
+`
+
