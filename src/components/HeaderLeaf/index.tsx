0e8474df26580c9e913ba4478450a5845c2003fd
eriks junior <eriksjunior13@gmail.com>
2022-10-24 20:12:50 -0300

    feat: ajustado alguns inputs

--- src/components/HeaderLeaf/index.tsx
+++ src/components/HeaderLeaf/index.tsx
@@ -5,16 +5,45 @@ import { Container } from './styles'
 export function HeaderLeaf() {
   return (
     <Container>
-      <Row sm={12} md={12} lg={12} xl={12}>
-        <Col sm={6} md={6} lg={6} xl={4}>
+      <Row xs={2} sm={12} md={12} lg={12} xl={12}>
+        <Col xs={5} sm={4} md={3} lg={3} xl={2}>
           <Form.Control
-            type="text"
-            placeholder='Natuzera da operação'
+            type="date"
+            placeholder='Data emissão'
             disabled
           />
         </Col>
 
-        <Col sm={6} md={6} lg={6} xl={4}>
+        <Col xs={7} sm={8} md={4} lg={5} xl={6}>
+          <Form.Control
+            type="text"
+            placeholder='Natuzera da operação'
+            disabled
+            className='naturezaSwitchToSmallerscreens'
+          />
+        </Col>
+
+        <Col sm={6} md={3} lg={2} xl={2}>
+          <Form.Control
+            type="text"
+            placeholder='Finalidade'
+            disabled
+            className='switchToSmallerscreens'
+          />
+        </Col>
+
+        <Col sm={6} md={2} lg={2} xl={2}>
+          <Form.Control
+            type="text"
+            placeholder='Tip. Emiss'
+            disabled
+            className='switchToSmallerscreens'
+          />
+        </Col>
+      </Row>
+
+      <Row xs={3} sm={12} md={12} lg={12} xl={12}>
+        <Col xs={12} sm={12} md={5} lg={5} xl={6}>
           <Form.Control
             type="text"
             placeholder='Chave NF-e'
@@ -22,27 +51,30 @@ export function HeaderLeaf() {
           />
         </Col>
 
-        <Col sm={4} md={4} lg={4} xl={1}>
+        <Col sm={4} md={2} lg={2} xl={2}>
           <Form.Control
             type="text"
             placeholder='Serie'
             disabled
+            className='switchToSmallerscreens'
           />
         </Col>
 
-        <Col sm={4} md={4} lg={4} xl={1}>
+        <Col sm={4} md={2} lg={2} xl={2}>
           <Form.Control
             type="text"
             placeholder='Nº NF-e'
             disabled
+            className='switchToSmallerscreens'
           />
         </Col>
 
-        <Col sm={4} md={4} lg={4} xl={2}>
+        <Col sm={4} md={3} lg={3} xl={2}>
           <Form.Control
-            type="date"
-            placeholder='Data emissão'
+            type="text"
+            placeholder='Status'
             disabled
+            className='switchToSmallerscreens'
           />
         </Col>
       </Row>
--- src/components/HeaderLeaf/styles.ts
+++ src/components/HeaderLeaf/styles.ts
@@ -4,23 +4,36 @@ export const Container = styled.div`
   width: 80vw;
   margin: 0;
   padding: 0;
-  height: 10vh;
+  height: 17vh;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  gap: 15px;
 
   @media screen and (max-width: 1199px){
   display: flex;
-  height: 15vh;
+  height: 16vh;
   gap: 10px;
   }
 
   @media screen and (max-width: 991px){
   display: flex;
-  height: 15vh;
+  height: 16vh;
   gap: 10px;
+  }
+
+  @media screen and (max-width: 767px){
+  display: flex;
+  flex-direction: column;
+  height: 31vh;
+  .switchToSmallerscreens {
+      margin-top: 10px;
+    }
   }
 
   @media screen and (max-width: 575px){
   display: flex;
-  height: 40vh;
+  height: 31vh;
   gap: 10px;
   }
 `
