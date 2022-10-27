6839ec5fdb9b748642154172729a8ed738fe2b21
leobraboo <leonardo.l.souza@ufv.br>
2022-10-27 20:59:51 -0300

    commit

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,13 +1,13 @@
 import { useCallback, useState } from 'react';
-import { Form, Offcanvas, Button, FloatingLabel , Tab , Tabs , Col, Row } from 'react-bootstrap';
+import { Form, Offcanvas, Button, FloatingLabel, Tab, Tabs, Col, Row } from 'react-bootstrap';
 
 import { ButtonBt, IconBiBox } from '../Styles/bootstrap';
 import { ContentIcon, Text } from '../Styles/general';
 
 import { Search } from '../Search/index';
 import ProductServices from '../../services/ProductService';
-import { IProducts  } from '../../interface/IProducts';
- 
+import { IProducts } from '../../interface/IProducts';
+
 
 
 export function ProductsModal() {
@@ -16,11 +16,11 @@ export function ProductsModal() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [ produtos , setProdutos ] = useState<IProducts>();
-  
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>)=> {
-    setProdutos({...produtos, [e.target.name]: e.target.value})
-  },[produtos]);
+  const [produtos, setProdutos] = useState<IProducts>();
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setProdutos({ ...produtos, [e.target.name]: e.target.value })
+  }, [produtos]);
 
   async function saveProducts() {
     const result = await ProductServices.save(produtos as IProducts)
@@ -43,70 +43,70 @@ export function ProductsModal() {
           <Offcanvas.Title style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bold", fontSize: "30px" }}  >Cadastro de Produto</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bolder" }}>
-        <Tabs
-          defaultActiveKey="Cadastro"
-          id="clientTabs"
-          className="mb-3"
-    >
-          <Tab eventKey="Cadastro" title="Cadastro">
-            <Form >
-            <FloatingLabel className="mb-4" label="Nome Do Produto">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="nome" placeholder="Nome Do Produto" />
-            </FloatingLabel >
-            <Row className="mb-3 ">
-              
-              <FloatingLabel as={Col} className="mb-4" label="Valor">
-                <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="valor" placeholder="Valor" />
-              </FloatingLabel>
+          <Tabs
+            defaultActiveKey="Cadastro"
+            id="clientTabs"
+            className="mb-3"
+          >
+            <Tab eventKey="Cadastro" title="Cadastro">
+              <Form >
+                <FloatingLabel className="mb-4" label="Nome Do Produto">
+                  <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="nome" placeholder="Nome Do Produto" />
+                </FloatingLabel >
+                <Row className="mb-3 ">
 
-              <FloatingLabel as={Col} className="mb-4" label="Valor de Venda">
-                <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="valorVenda" placeholder="Valor de Venda" />
-              </FloatingLabel>
-              
-            </Row>
+                  <FloatingLabel as={Col} className="mb-4" label="Valor">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="valor" placeholder="Valor" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="Unidade">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="unidade" placeholder="Unidade" />
-            </FloatingLabel>
+                  <FloatingLabel as={Col} className="mb-4" label="Valor de Venda">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="valorVenda" placeholder="Valor de Venda" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="Descrição">
-              <Form.Control as="textarea" style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="descricao" placeholder="Descrição" />
-            </FloatingLabel>
+                </Row>
 
-            <FloatingLabel className="mb-4" label="Código de Barras">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="codBarras" placeholder="Código de Barras" />
-            </FloatingLabel>
-
-            <FloatingLabel className="mb-4" label="Código de Referência">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="codReferencia" placeholder="Código de Referência" />
-            </FloatingLabel>
-
-            <Row className="mb-3">
-                <FloatingLabel as={Col} className="mb-4" label="Estoque">
-                  <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="estoque" placeholder="Estoque" />
+                <FloatingLabel className="mb-4" label="Unidade">
+                  <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="unidade" placeholder="Unidade" />
                 </FloatingLabel>
 
-                <FloatingLabel as={Col} className="mb-4" label="Estoque Mínimo">
-                  <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange}  name="estoqueMin" placeholder="Estoque Mínimo" />
+                <FloatingLabel className="mb-4" label="Descrição">
+                  <Form.Control as="textarea" style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="descricao" placeholder="Descrição" />
                 </FloatingLabel>
-            </Row>
-            
 
-            <div className="d-grid gap-2">
-              <Button variant="" type="button" onClick={saveProducts} size='lg' style={{background:"BlueViolet" , color:"whitesmoke"}}>
-                Cadastrar
-              </Button>
-            </div>
-            </Form>
-          </Tab>
-          <Tab eventKey="Pesquisar" title="Pesquisar">
-            <Search />
-          </Tab>
+                <FloatingLabel className="mb-4" label="Código de Barras">
+                  <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="codBarras" placeholder="Código de Barras" />
+                </FloatingLabel>
 
-        </Tabs>
+                <FloatingLabel className="mb-4" label="Código de Referência">
+                  <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="codReferencia" placeholder="Código de Referência" />
+                </FloatingLabel>
+
+                <Row className="mb-3">
+                  <FloatingLabel as={Col} className="mb-4" label="Estoque">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="estoque" placeholder="Estoque" />
+                  </FloatingLabel>
+
+                  <FloatingLabel as={Col} className="mb-4" label="Estoque Mínimo">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} type="text" onChange={handleChange} name="estoqueMin" placeholder="Estoque Mínimo" />
+                  </FloatingLabel>
+                </Row>
 
 
-          
+                <div className="d-grid gap-2">
+                  <Button variant="" type="button" onClick={saveProducts} size='lg' style={{ background: "BlueViolet", color: "whitesmoke" }}>
+                    Cadastrar
+                  </Button>
+                </div>
+              </Form>
+            </Tab>
+            <Tab eventKey="Pesquisar" title="Pesquisar">
+              <Search />
+            </Tab>
+
+          </Tabs>
+
+
+
         </Offcanvas.Body>
       </Offcanvas>
     </>
