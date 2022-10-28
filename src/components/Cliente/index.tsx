a0148824f4f7dd9010a0484c6db71d822395bfcd
leobraboo <leonardo.l.souza@ufv.br>
2022-10-28 18:39:24 -0300

    REFACTOR : layout label client

--- src/components/Cliente/index.tsx
+++ src/components/Cliente/index.tsx
@@ -1,11 +1,11 @@
-import { useState , useCallback } from 'react';
+import { useState, useCallback } from 'react';
 
-import { Form, Button , FloatingLabel , Offcanvas , Tab , Tabs } from 'react-bootstrap';
+import { Form, Button, FloatingLabel, Offcanvas, Tab, Tabs } from 'react-bootstrap';
 
 import { ButtonBt, IconBiUser } from '../Styles/bootstrap';
 import { ContentIcon, Text } from '../Styles/general';
 import { Search } from '../Search/index';
-
+import { contentOffcanvas } from './styles'
 import ClienteService from '../../services/ClienteService';
 import { ICliente } from '../../interface/ICliente';
 
@@ -16,10 +16,10 @@ export function ClientModal() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [client , setClient] = useState<ICliente>();
+  const [client, setClient] = useState<ICliente>();
 
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>)=>{
-    setClient ({ ...client , [e.target.name]: e.target.value})
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setClient({ ...client, [e.target.name]: e.target.value })
   }, [client]);
 
   async function saveClient() {
@@ -37,101 +37,118 @@ export function ClientModal() {
           Clientes
         </Text>
       </ButtonBt>
+        
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header style={{ background: "#1C1C1C", color: "whitesmoke" }} closeButton>
+            <Offcanvas.Title style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bold", fontSize: "30px" }}>Cadastre o Cliente</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bolder" }}>
+            <Tabs
+              defaultActiveKey="Cadastro"
+              id="clientTabs"
+              className="mb-3"
+            >
+              <Tab eventKey="Cadastro" title="Cadastro">
+                <Form >
+                  <FloatingLabel className="mb-4" label="Nome do Cliente">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="nome" type="text" />
+                  </FloatingLabel>
 
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header style={{ background: "#1C1C1C", color: "whitesmoke" }} closeButton>
-          <Offcanvas.Title style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bold", fontSize: "30px" }}>Cadastre o Cliente</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bolder" }}>
-          <Tabs
-            defaultActiveKey="Cadastro"
-            id="clientTabs"
-            className="mb-3"
-    >
-            <Tab eventKey="Cadastro" title="Cadastro">
-              <Form >
-            <FloatingLabel className="mb-4" label="Nome do Cliente">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="nome" type="text" placeholder="Nome do Cliente:" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="CPF/CNPJ">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="cpfCnpj" type="text" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="CPF/CNPJ">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="cpfCnpj" type="text" placeholder="CPF/CNPJ" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="Endereço">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="endereco" type="text" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="Endereço">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="endereco" type="text" placeholder="Endereço" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="Número">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="numero" type="text" />
+                  </FloatingLabel>
+
+                  <FloatingLabel className="mb-4" label="Complemento">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="complemento" type="text" />
+                  </FloatingLabel>
+
+                  <FloatingLabel className="mb-4" label="Bairro">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="bairro" type="text" />
+                  </FloatingLabel>
+
+                  <FloatingLabel className="mb-4" label="Cidade">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="cidade" type="text" />
+                  </FloatingLabel>
 
 
-            <FloatingLabel className="mb-4" label="Cidade">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="cidade" type="text" placeholder="Cidade" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="Estado">
+                    <Form.Select style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bolder" }} defaultValue="">
+                      <option value="">Selecione...</option>
+                      <option value="AC">Acre</option>
+                      <option value="AL">Alagoas</option>
+                      <option value="AP">Amapá</option>
+                      <option value="AM">Amazonas</option>
+                      <option value="BA">Bahia</option>
+                      <option value="CE">Ceará</option>
+                      <option value="DF">Distrito Federal</option>
+                      <option value="ES">Espirito Santo</option>
+                      <option value="GO">Goiás</option>
+                      <option value="MA">Maranhão</option>
+                      <option value="MS">Mato Grosso do Sul</option>
+                      <option value="MT">Mato Grosso</option>
+                      <option value="MG">Minas Gerais</option>
+                      <option value="PA">Pará</option>
+                      <option value="PB">Paraíba</option>
+                      <option value="PR">Paraná</option>
+                      <option value="PE">Pernambuco</option>
+                      <option value="PI">Piauí</option>
+                      <option value="RJ">Rio de Janeiro</option>
+                      <option value="RN">Rio Grande do Norte</option>
+                      <option value="RS">Rio Grande do Sul</option>
+                      <option value="RO">Rondônia</option>
+                      <option value="RR">Roraima</option>
+                      <option value="SC">Santa Catarina</option>
+                      <option value="SP">São Paulo</option>
+                      <option value="SE">Sergipe</option>
+                      <option value="TO">Tocantins</option>
+                    </Form.Select>
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="Bairro">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="bairro" type="text" placeholder="Bairro" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="CEP">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="cep" type="text" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="Estado">
-              <Form.Select style={{ background: "#1C1C1C", color: "LightGrey", fontWeight: "bolder" }} defaultValue="">
-                <option value="">Selecione...</option>
-                <option value="AC">Acre</option>
-                <option value="AL">Alagoas</option>
-                <option value="AP">Amapá</option>
-                <option value="AM">Amazonas</option>
-                <option value="BA">Bahia</option>
-                <option value="CE">Ceará</option>
-                <option value="DF">Distrito Federal</option>
-                <option value="ES">Espirito Santo</option>
-                <option value="GO">Goiás</option>
-                <option value="MA">Maranhão</option>
-                <option value="MS">Mato Grosso do Sul</option>
-                <option value="MT">Mato Grosso</option>
-                <option value="MG">Minas Gerais</option>
-                <option value="PA">Pará</option>
-                <option value="PB">Paraíba</option>
-                <option value="PR">Paraná</option>
-                <option value="PE">Pernambuco</option>
-                <option value="PI">Piauí</option>
-                <option value="RJ">Rio de Janeiro</option>
-                <option value="RN">Rio Grande do Norte</option>
-                <option value="RS">Rio Grande do Sul</option>
-                <option value="RO">Rondônia</option>
-                <option value="RR">Roraima</option>
-                <option value="SC">Santa Catarina</option>
-                <option value="SP">São Paulo</option>
-                <option value="SE">Sergipe</option>
-                <option value="TO">Tocantins</option>
-              </Form.Select>
-            </FloatingLabel>
+                  <FloatingLabel className="mb-3" label="Telefone">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="telefone" type="text" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="CEP">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="cep" type="text" placeholder="CEP" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-3" label="Celular">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="celular" type="text" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-4" label="Email">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="email" type="email" placeholder="Email" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="Email">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="email" type="text" />
+                  </FloatingLabel>
 
-            <FloatingLabel className="mb-3" label="Telefone">
-              <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="telefone" type="text" placeholder="Telefone" />
-            </FloatingLabel>
+                  <FloatingLabel className="mb-4" label="Observação">
+                    <Form.Control style={{ background: "#1C1C1C", color: "whitesmoke" }} onChange={handleChange} name="observacao" type="text" />
+                  </FloatingLabel>
 
-            <div className="d-grid gap-2">
-              <Button variant="" onClick={saveClient} type="button" size='lg' style={{ background: "BlueViolet", color: "whitesmoke" }}>
-                Cadastrar
-              </Button>
-            </div>
 
-              </Form>
-            </Tab>
-            <Tab eventKey="Pesquisar" title="Pesquisar">
-              <Search />
-            </Tab>
+                  <div className="d-grid gap-2">
+                    <Button variant="" onClick={saveClient} type="button" size='lg' style={{ background: "BlueViolet", color: "whitesmoke" }}>
+                      Cadastrar
+                    </Button>
+                  </div>
 
-          </Tabs>
-          
-        </Offcanvas.Body>
-      </Offcanvas>
+                </Form>
+              </Tab>
+              <Tab eventKey="Pesquisar" title="Pesquisar">
+                <Search />
+              </Tab>
+
+            </Tabs>
+
+          </Offcanvas.Body>
+        </Offcanvas>
     </>
   );
 }
--- src/components/Cliente/styles.ts
+++ src/components/Cliente/styles.ts
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+import { Form, Button, FloatingLabel, Offcanvas, Tab, Tabs } from 'react-bootstrap';
+
+
+
+export const contentOffcanvas = styled(Offcanvas)`
+    .offcanvas-start {
+        background: "red";
+}
+`
