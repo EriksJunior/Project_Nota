ef16b370fb39611ebdcc485ac78019a2ad6f3fb6
eriks junior <eriksjunior13@gmail.com>
2022-11-20 14:51:27 -0300

    REFACTOR: removido style inline do modal de pesquisa da nota

--- src/shared/components/Leaf/components/Modals/ModalSearch/index.tsx
+++ src/shared/components/Leaf/components/Modals/ModalSearch/index.tsx
@@ -5,14 +5,14 @@ import { Search } from "../../../../Search"
 import { OffcanvasBt, OffcanvasHeaderBt, OffcanvasTitleBt, OffcanvasBodyBt } from "../../../../Styles/bootstrap"
 
 export function ModalSearch(){
-  const {handleShow, handleClose, show} = useContext(LeafContext) as {handleShow: () => void, handleClose: () => void, show: Boolean}
+  const { handleClose, show} = useContext(LeafContext) as {handleShow: () => void, handleClose: () => void, show: Boolean}
   return(
       <>
-      <OffcanvasBt show={show} onHide={handleClose} placement={'end'} style={{ width: "50vw" }}>
-        <OffcanvasHeaderBt style={{ backgroundColor: "blueViolet" }} closeButton>
-          <OffcanvasTitleBt style={{ color: "white", fontWeight: "bold", fontSize: "36px", justifyContent: "center", paddingLeft: "50px" }}>Pesquisar Notas</OffcanvasTitleBt>
+      <OffcanvasBt show={show} onHide={handleClose} placement={'end'}>
+        <OffcanvasHeaderBt closeButton>
+          <OffcanvasTitleBt>Pesquisar Notas</OffcanvasTitleBt>
         </OffcanvasHeaderBt>
-        <OffcanvasBodyBt style={{ background: "#f5f5f5", color: "LightGrey", fontWeight: "bolder" }}>
+        <OffcanvasBodyBt>
           Some text as placeholder. In real life you can have the elements you
           have chosen. Like, text, images, lists, etc.
         </OffcanvasBodyBt>
