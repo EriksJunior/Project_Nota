91fae1fa16835b8707dbb7f68acce4f9bf52cff9
eriks junior <eriksjunior13@gmail.com>
2022-11-20 17:18:09 -0300

    REFACTOR: Ajustado algumas coisas no modal

--- src/shared/components/Leaf/components/Modals/ModalSearch/index.tsx
+++ src/shared/components/Leaf/components/Modals/ModalSearch/index.tsx
@@ -1,20 +1,38 @@
 import { useContext } from "react"
 import { LeafContext } from "../../../../../context/leaf/leaf"
 import { Search } from "../../../../Search"
+import { LeafTable } from "../../LeafTable"
 
-import { OffcanvasBt, OffcanvasHeaderBt, OffcanvasTitleBt, OffcanvasBodyBt } from "./styles"
+import { OffcanvasBt, OffcanvasHeaderBt, OffcanvasTitleBt, OffcanvasBodyBt, FormControlBt, ButtonBt, RowBt, ColBt, FormSelectBt } from "./styles"
 
-export function ModalSearch(){
-  const { handleClose, show} = useContext(LeafContext) as {handleShow: () => void, handleClose: () => void, show: Boolean}
-  return(
-      <>
+export function ModalSearch() {
+  const { handleClose, show } = useContext(LeafContext) as { handleShow: () => void, handleClose: () => void, show: Boolean }
+  return (
+    <>
       <OffcanvasBt show={show} onHide={handleClose} placement={'end'}>
         <OffcanvasHeaderBt closeButton>
           <OffcanvasTitleBt>Pesquisar Notas</OffcanvasTitleBt>
         </OffcanvasHeaderBt>
         <OffcanvasBodyBt>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+          <RowBt>
+              <RowBt  xs={12}>
+                  <ColBt xs={6} sm={6} md={6} lg={6} xl={6}>
+                    <FormControlBt className="me-auto" placeholder="Faça sua pesquisa" />
+                  </ColBt>
+
+                  <ColBt xs={6} sm={4} md={4} lg={4} xl={4}>
+                    <FormSelectBt></FormSelectBt>
+                  </ColBt>
+
+                  <ColBt className={"btnPesquisar"} xs={2} sm={2} md={2} lg={2} xl={2}>
+                    <ButtonBt variant="secondary">Pesquisar</ButtonBt>
+                  </ColBt>
+              </RowBt>
+          </RowBt>
+
+          <RowBt className="mt-5">
+            <LeafTable />
+          </RowBt>
         </OffcanvasBodyBt>
       </OffcanvasBt>
     </>
--- src/shared/components/Leaf/components/Modals/ModalSearch/styles.ts
+++ src/shared/components/Leaf/components/Modals/ModalSearch/styles.ts
@@ -1,8 +1,28 @@
 import styled from "styled-components";
-import { Offcanvas, OffcanvasHeader, OffcanvasTitle, OffcanvasBody } from 'react-bootstrap';
+import { FormControl, Offcanvas, OffcanvasHeader, OffcanvasTitle, OffcanvasBody, Button, Row, Col, FormSelect } from 'react-bootstrap';
 
 export const OffcanvasBt = styled(Offcanvas)`
 width: 50%!important;
+
+  @media screen and (max-width: 1000px){
+    width: 60%!important;
+  }
+
+  @media screen and (max-width: 848px){
+    width: 70%!important;
+  }
+
+  @media screen and (max-width: 727px){
+    width: 100%!important;
+  }
+
+  @media screen and (max-width: 575px){
+    .btnPesquisar{
+      margin-top: 10px;
+    }
+  }
+
+
 `
 
 export const OffcanvasHeaderBt = styled(OffcanvasHeader)`
@@ -21,4 +41,25 @@ export const OffcanvasBodyBt = styled(OffcanvasBody)`
 background-color: #f5f5f5!important;
 color: LightGrey!important;
 font-weight: bolder!important;
+`
+
+export const FormControlBt = styled(FormControl)`
+
+`
+
+export const ButtonBt = styled(Button)`
+
+`
+
+export const RowBt = styled(Row)`
+`
+
+export const ColBt = styled(Col)`
+
+`
+
+export const FormSelectBt = styled(FormSelect)`
+  color: black!important;
+  font-weight: bolder!important;
+  height: 38px!important;
 `
